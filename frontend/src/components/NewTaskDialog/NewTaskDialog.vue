58a5dc7ac7ba5de7d42e906f0ddb9fcde61bc6c9
ldaahir <162431181+ldaahir@users.noreply.github.com>
2025-10-19 13:26:49 +0300

    fixed

--- frontend/src/components/NewTaskDialog/NewTaskDialog.vue
+++ frontend/src/components/NewTaskDialog/NewTaskDialog.vue
@@ -1,65 +1,96 @@
 <template>
-  <Dialog
-    :options="{ title: 'New Task' }"
-    :disableOutsideClickToClose="disableOutsideClickToClose"
-    v-model="showDialog"
-  >
+  <Dialog :options="{ title: 'New Task' }" :disableOutsideClickToClose="disableOutsideClickToClose"
+    v-model="showDialog">
+    <!-- Body -->
     <template #body-content>
       <div class="space-y-4" v-if="newTask">
-        <FormControl
-          label="Title"
-          v-model="newTask.doc.title"
-          autocomplete="off"
-          required
-          ref="titleInput"
-          @keydown.enter="onCreateClick"
-        />
-        <FormControl
-          label="Description"
-          type="textarea"
-          v-model="newTask.doc.description"
-          @keydown.enter="onCreateClick"
-        />
+
+
+
+        <!-- Title -->
+        <FormControl label="Title" v-model="newTask.doc.title" autocomplete="off" required ref="titleInput"
+          @keydown.enter="onCreateClick" />
+
+        <!-- Project -->
+        <Autocomplete placeholder="Project" :options="spaceOptions" v-model="newTask.doc.project">
+          <template #prefix>
+            <div class="mr-2 leading-4 font-[emoji]" v-if="newTask.doc.project">
+              {{ useSpace(newTask.doc.project?.value ?? newTask.doc.project).value.icon }}
+            </div>
+          </template>
+          <template #item-prefix="{ option }">
+            <div class="leading-4 font-[emoji]">{{ option.icon }}</div>
+          </template>
+        </Autocomplete>
+
+        <!-- Sprint -->
+        <Autocomplete placeholder="Sprint" :options="SprintOptions" v-model="newTask.doc.sprint" />
+
+        <!-- Description -->
+        <FormControl label="Description" type="textarea" v-model="newTask.doc.description"
+          @keydown.enter="onCreateClick" />
+
+        <!-- Assigned user + Due date -->
         <div class="grid grid-cols-2 gap-2">
-          <Autocomplete
-            placeholder="Assign a user"
-            :options="assignableUsers"
-            v-model="newTask.doc.assigned_to"
-          >
-            <template #prefix>
-              <UserAvatar
-                class="mr-2"
-                size="xs"
-                :user="newTask.doc.assigned_to?.value ?? newTask.doc.assigned_to"
-              />
-            </template>
-            <template #item-prefix="{ option }">
-              <UserAvatar size="xs" :user="option.value" />
-            </template>
-          </Autocomplete>
+          <Autocomplete placeholder="Assign a user" :options="assignableUsers" v-model="newTask.doc.assigned_to" />
           <TextInput type="date" placeholder="Set due date" v-model="newTask.doc.due_date" />
-          <Autocomplete placeholder="Project" :options="spaceOptions" v-model="newTask.doc.project">
-            <template #prefix>
-              <div class="mr-2 leading-4 font-[emoji]" v-if="newTask.doc.project">
-                {{ useSpace(newTask.doc.project?.value ?? newTask.doc.project).value.icon }}
-              </div>
-            </template>
-            <template #item-prefix="{ option }">
-              <div class="leading-4 font-[emoji]">{{ option.icon }}</div>
-            </template>
-          </Autocomplete>
-          <Dropdown class="w-full" :options="statusOptions()">
-            <Button>
-              <template #prefix v-if="newTask.doc.status">
-                <TaskStatusIcon :status="newTask.doc.status" />
-              </template>
-              {{ newTask.doc.status }}
-            </Button>
-          </Dropdown>
         </div>
+
+
+
+
+        <!-- <Dropdown class="w-full -z-30" :options="typeOptions()" :teleport="true">
+          <Button class="w-full justify-start">
+            <template #prefix v-if="newTask.doc.type">
+              <TaskStatusIcon :type="newTask.doc.type" />
+            </template>
+            {{ newTask.doc.type || 'Select type' }}
+          </Button>
+        </Dropdown> -->
+
+
+        <Dropdown 
+        :options="[
+          {
+            label: 'Edit',
+            icon: 'edit',
+          },
+          {
+            label: 'Delete', icon: 'trash-2'
+
+          }, {
+            label: 'Edit',
+            icon: 'edit',
+          },
+          {
+            label: 'Delete', icon: 'trash-2'
+
+          }, {
+            label: 'Edit',
+            icon: 'edit',
+          },
+          {
+            label: 'Delete', icon: 'trash-2'
+
+          }, {
+            label: 'Edit',
+            icon: 'edit',
+          },
+          {
+            label: 'Delete', icon: 'trash-2'
+
+          },
+        ]"
+        class="bg-red-500 z-index: 7777; "
+         />
+
+
+        <!-- Error message -->
         <ErrorMessage class="mt-2" :message="newTask.error" />
       </div>
     </template>
+
+    <!-- Actions -->
     <template #actions>
       <Button class="w-full relative" variant="solid" @click="onCreateClick">
         Create
@@ -70,9 +101,19 @@
     </template>
   </Dialog>
 </template>
+
 <script setup lang="ts">
-import { computed, h, useTemplateRef, watch } from 'vue'
-import { Dialog, FormControl, Autocomplete, Dropdown, TextInput } from 'frappe-ui'
+import { ref, computed, h, watch, useTemplateRef } from 'vue'
+import {
+  Dialog,
+  FormControl,
+  Autocomplete,
+  Dropdown,
+  TextInput,
+  ErrorMessage,
+  Button,
+  call,
+} from 'frappe-ui'
 import TaskStatusIcon from './TaskStatusIcon.vue'
 import { activeUsers } from '@/data/users'
 import { GPTask } from '@/types/doctypes'
@@ -82,62 +123,104 @@ import { useSpace } from '@/data/spaces'
 import KeyboardShortcut from '../KeyboardShortcut.vue'
 
 const titleInput = useTemplateRef('titleInput')
-let spaceOptions = useGroupedSpaceOptions({ filterFn: (space) => !space.archived_at })
 
+// --- Project options
+const spaceOptions = useGroupedSpaceOptions({
+  filterFn: (space) => !space.archived_at,
+})
+
+// --- Sprint options (dynamic)
+const SprintOptions = ref([])
+
+async function fetchSprints(projectName?: string) {
+  if (!projectName) {
+    SprintOptions.value = []
+    return
+  }
+
+  try {
+    const res = await call('frappe.client.get_list', {
+      doctype: 'Sprint',
+      filters: { project: projectName },
+      fields: ['name', 'title'],
+    })
+
+    SprintOptions.value = (res || []).map((sprint: any) => ({
+      label: sprint.title || sprint.name,
+      value: sprint.name,
+    }))
+  } catch (err) {
+    console.error('Error fetching sprints:', err)
+  }
+}
+
+// Watch for project changes
+watch(
+  () => newTask.value?.doc?.project,
+  (project) => {
+    const projectName = project?.value ?? project
+    fetchSprints(projectName)
+  },
+  { immediate: true },
+)
+
+// --- Status options
 function statusOptions() {
   return (['Backlog', 'Todo', 'In Progress', 'Done', 'Canceled'] as GPTask['status'][]).map(
-    (status) => {
-      return {
-        icon: () => h(TaskStatusIcon, { status }),
-        label: status,
-        onClick: () => {
-          if (newTask.value) {
-            newTask.value.doc.status = status
-          }
-        },
-      }
-    },
+    (status) => ({
+      icon: () => h(TaskStatusIcon, { status }),
+      label: status,
+      onClick: () => {
+        if (newTask.value) newTask.value.doc.status = status
+      },
+    }),
   )
 }
 
-const assignableUsers = computed(() => {
-  return activeUsers.value.map((user) => ({
+// --- Type options
+function typeOptions() {
+  return (['Task', 'Bug'] as GPTask['type'][]).map((type) => ({
+    icon: () => h(TaskStatusIcon, { type }),
+    label: type,
+    onClick: () => {
+      if (newTask.value) newTask.value.doc.type = type
+    },
+  }))
+}
+
+// --- Assignable users
+const assignableUsers = computed(() =>
+  activeUsers.value.map((user) => ({
     label: user.full_name,
     value: user.name,
-  }))
-})
+  })),
+)
 
+// --- Create task
 function onCreateClick(e: KeyboardEvent) {
-  if (e instanceof KeyboardEvent && !(e.ctrlKey || e.metaKey)) {
-    return
-  }
-
-  if (!newTask.value) return
-  if (!newTask.value.doc.title) {
+  if (e instanceof KeyboardEvent && !(e.ctrlKey || e.metaKey)) return
+  if (!newTask.value?.doc.title) {
     newTask.value.error = new Error('Task title is required')
     return
   }
+
   newTask.value.doc.assigned_to = newTask.value.doc.assigned_to?.value
+  newTask.value.doc.project = newTask.value.doc.project?.value
+  newTask.value.doc.sprint = newTask.value.doc.sprint?.value
 
-  if (newTask.value.doc.project?.value) {
-    newTask.value.doc.project = newTask.value.doc.project?.value
-  }
-
-  return newTask.value.submit().then((doc) => {
+  newTask.value.submit().then((doc) => {
     showDialog.value = false
     _onSuccess.value(doc)
   })
 }
 
-let disableOutsideClickToClose = computed(() => {
-  return newTask.value?.loading || newTask.value?.doc?.title != ''
-})
+// --- Prevent accidental close
+const disableOutsideClickToClose = computed(
+  () => newTask.value?.loading || newTask.value?.doc?.title != '',
+)
 
+// --- Auto-focus on open
 watch(showDialog, (val) => {
-  if (val) {
-    setTimeout(() => {
-      titleInput.value.$el?.querySelector('input')?.focus()
-    }, 100)
-  }
+  if (val) setTimeout(() => titleInput.value.$el?.querySelector('input')?.focus(), 100)
 })
 </script>
--- gameplan/gameplan/doctype/sprint/sprint.js
+++ gameplan/gameplan/doctype/sprint/sprint.js
@@ -1,8 +1,18 @@
-// Copyright (c) 2025, Frappe Technologies Pvt Ltd and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Sprint', {
 
-// frappe.ui.form.on("Sprint", {
-// 	refresh(frm) {
+    project: async function(frm) {
+        if (!frm.doc.project_name) return;
+        const project_code = frm.doc.project_name.toLowerCase().replace(/\s+/g, '-');
+        console.log(project_code)
+        const existing_sprints = await frappe.db.get_list('Sprint', {
+            filters: { project: frm.doc.project },
+            fields: ['name'],
+            limit: 1000
+        });
+        const sprint_count = existing_sprints.length + 1;
+        const sprint_number = String(sprint_count).padStart(3, '0');
+        const sprint_name = `${project_code}-sprint-${sprint_number}`;
+        frm.set_value('sprint_name', sprint_name);
+    }
+});
 
-// 	},
-// });
